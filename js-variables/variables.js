d55ba3c2148e18ed51a18e42ce02faf05c88dc95
vaasilik <vs.marina49@gmail.com>
2023-09-20 23:48:29 +0300

    my first JS experience

--- js-variables/variables.js
+++ js-variables/variables.js
@@ -1,3 +1,28 @@
 let adminName = "Maryna";
-adminName = "awersome"; 
+adminName = 10; 
 console.log(typeof adminName);
+
+let userName = "Maxim";
+userName = 20;
+console.log(typeof adminName);
+
+
+let title;
+title = "awersome";
+console.log(title);
+
+let clickButton;
+clickButton = "push";
+console.log(clickButton);
+
+let subTitle; 
+subTitle = "interesting";
+console.log(subTitle);
+
+console.log(`Javascript is: ${"title"} ${"clickButton"} ${"subTitle"}`);
+
+
+let finishButton = "stoppp";
+finishButton = "stop";
+
+console.log(finishButton);
