9953d04c87e6c9dcf3f7c6f24a3f56962bdc6893
vaasilik <vs.marina49@gmail.com>
2023-09-21 09:45:55 +0300

    fix for 1st JS experience

---
+++ js-variables/variables.js
@@ -0,0 +1,21 @@
+let adminName = "Maryna"; 
+console.log(typeof adminName);
+
+let userName = "Maxim";
+console.log(typeof adminName);
+
+let title = "awersome";
+console.log(title);
+
+let clickButton = "push";
+console.log(clickButton);
+
+let subTitle = "interesting";
+console.log(subTitle);
+
+console.log(`Javascript is: ${"title"} ${"clickButton"} ${"subTitle"}`);
+
+let finishButton = "stoppp";
+finishButton = "stop";
+
+console.log(finishButton);
